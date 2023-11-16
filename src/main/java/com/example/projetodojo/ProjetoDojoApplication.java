01f45b3cde723d4eea340a97e5fb48793a348b23
carolina <lincklopescarol@gmail.com>
2023-11-16 16:39:37 -0300

    continuando o dojo em aula. Alunos: Bianca, Carolina, Augusto, Denis e Eduardo Ferrão

---
+++ src/main/java/com/example/projetodojo/ProjetoDojoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetodojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetodojo/ProjetoDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetodojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
